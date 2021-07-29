9fbba61522c144af590182ec6dd2d82f35122929
Kenan Yararbas <kenanyararbas@gmail.com>
2021-07-29 13:16:01 +0300

    Crawler added

--- main.py
+++ main.py
@@ -1,34 +1,71 @@
+from urllib.parse import urlparse
 import requests
 import argparse
 import sys
 import asyncio
 import pprint
+from bs4 import BeautifulSoup as bs
 
+# Arguments and argument Parsers
 parser = argparse.ArgumentParser(description="DAST Analysis tools -h for help")
 parser.add_argument('-u', '--url', type=str, required=True)
 parser.add_argument('-c', '--cookie', type=str)
 parser.add_argument('-ht' '--httpheader', type=str)
 parsed_args = parser.parse_args()
-enumarated_protocols = ["ftp://", "smtp://", "http://", "https://"]
 current_protocols = ["http://", "https://"]
 
-def add_protocol(protocol):
-    if current_protocols.append(protocol):
-        sys.stdout.write("Protocol Added:{}".format(protocol))
-        return True
-    else:
-        sys.stdout.write("Error while processing the protocol:{}".format(protocol))
-        return False
+# Global Variables
+urls = []
 
-def get_site():
-    if parsed_args.url:
-        site = parsed_args.url
-        for i in protocols:
-            final_url = i+site
-            returned = requests.request(url=final_url, method="GET")
-            pprint.pprint(returned.headers)
-    else:
-        print("No such argument")
+
+def get_all_element(url, element_type):
+    """Given a `url`, it returns an instance based on the element type from the HTML content"""
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all(element_type.lower())
+
+
+def get_headers(url):
+    """Given a 'url' it returns the http headers"""
+    return requests.get(url).headers
+
+
+def get_cookies(url):
+    """Given a 'url' it returns the cookies"""
+    session = requests.session()
+    cookies = session.get(url).cookies
+    cookie_dict = cookies.get_dict()
+    print(cookie_dict)
+
+
+""" Crawl (Spidering) the website recursively will be used with parameter existence in terms of sqli and other code
+    injections
+"""
+
+
+def scrape(site):
+    # getting the request from url
+    r = requests.get(site)
+
+    # converting the text
+    s = bs(r.text, "html.parser")
+
+    for i in s.find_all("a"):
+
+        href = i.attrs['href']
+
+        if href.startswith("/"):
+            site = site + href
+
+        else:
+            site = (str(urlparse(site).scheme + "://" + urlparse(site).netloc) + "/" + href)
+            print(site)
+
+            if site not in urls:
+                urls.append(site)
+                print(site)
+                # calling it self
+                scrape(site)
+
 
 if __name__ == '__main__':
-    get_site()
+    scrape(parsed_args.url)
